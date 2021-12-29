03b3269ef4a02d1c9f41edd049116c4ee8e3a638
Johan Tovar <jtovarto@gmail.com>
2021-12-29 14:37:52 -0400

    Make properties request (working in progress)

--- tests/EasyBrokerTest.php
+++ tests/EasyBrokerTest.php
@@ -3,12 +3,55 @@
 namespace EasyBroker\Test;
 
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Client;
+use EasyBroker\EasyBroker;
 
 class EasyBrokerTest extends TestCase
 {
-    /** @test */
-    public function itCanTestSomething()
+    /** @var \Spatie\Geocoder\Geocoder */
+    protected $easyBroker;
+
+    public function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        $client = new Client();
+
+        $this->easyBroker = new EasyBroker($client);
+        
+
+        if (!$apiKey = $this->getApiKey()) {
+            $this->markTestSkipped('No API key was provided.');
+
+            return;
+        }
+
+        $this->easyBroker->setApiKey($apiKey);
     }
+
+    /** @test */
+    public function it_can_get_a_city()
+    {
+        $result = $this->easybroker->getCoordinatesForAddress('Antwerp');
+
+        $this->assertArrayHasKey('lat', $result);
+        $this->assertArrayHasKey('lng', $result);
+        $this->assertArrayHasKey('accuracy', $result);
+        $this->assertArrayHasKey('formatted_address', $result);
+        $this->assertArrayHasKey('viewport', $result);
+    }
+
+    protected function getApiKey()
+    {  
+        $apiKeyPath = __DIR__ . '/../.apiKey';
+
+        if (!file_exists($apiKeyPath)) {
+            return;
+        }
+
+        return file_get_contents($apiKeyPath);
+
+        
+    }
+
 }
